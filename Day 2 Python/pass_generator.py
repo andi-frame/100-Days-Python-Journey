ac327137be539a62e9c6420e0778299e3da9b29d
andi-frame <132210040+andi-frame@users.noreply.github.com>
2023-05-02 00:26:16 +0800

    Add files via upload
    Have a lot of activity today, but still manage to make one project. Hope yall like it.

---
+++ Day 2 Python/pass_generator.py
@@ -0,0 +1,42 @@
+import secrets
+import string
+import random
+
+# MEMO
+letters = list(string.ascii_letters)
+numbers = list(string.digits)
+symbols = list(string.punctuation)
+
+# USER_AMOUNT_INPUT
+print("Welcome to the Password Generator by Andi-Frame")
+print("Please fill this specification on your password:")
+letters_amount = int(input("How many letters: "))
+numbers_amount = int(input("How many numbers: "))
+symbols_amount = int(input("How many symbols: "))
+
+# FETCH
+pass_letters = ''.join(secrets.choice(letters) for i in range(letters_amount))
+pass_numbers = ''.join(secrets.choice(numbers) for i in range(numbers_amount))
+pass_symbols = ''.join(secrets.choice(symbols) for i in range(symbols_amount))
+
+# THE_PASSWORD
+password = list(set(pass_letters + pass_numbers + pass_symbols))
+print(*password, sep = '')
+
+# REGENERATE
+regenerate = int(input("Do you want to regenerate your password (1 for yes, 0 for no)? "))
+while regenerate == 1:
+    pass_letters = ''.join(secrets.choice(letters) for i in range(letters_amount))
+    pass_numbers = ''.join(secrets.choice(numbers) for i in range(numbers_amount))
+    pass_symbols = ''.join(secrets.choice(symbols) for i in range(symbols_amount))
+    new_password = list(set(pass_letters + pass_numbers + pass_symbols))
+    print(*new_password, sep = '')
+    regenerate = int(input("Do you want to regenerate your password (1 for yes, 0 for no)? "))
+    
+
+# SHUFFLE
+shuffle = int(input("Want to reshuffle (1 for yes, 0 for no)? "))
+while shuffle == 1:
+    random.shuffle(password)
+    print(*password, sep = '')
+    shuffle = int(input("Want to reshuffle (1 for yes, 0 for no)? "))
