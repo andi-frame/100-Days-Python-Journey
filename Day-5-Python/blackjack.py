2ed00fbc0ffeb86a6e4f1b71b2a6d147a66ed44d
andi-frame <132210040+andi-frame@users.noreply.github.com>
2023-05-05 17:37:12 +0800

    Update blackjack.py
    This change was made on day 6 of #100DaysOfCode

--- Day-5-Python/blackjack.py
+++ Day-5-Python/blackjack.py
@@ -1,14 +1,17 @@
 import os
 import random
-print('''
-===============================
+text = '''
+================================
 !! WELCOME TO THE BLACKJACK !!
-===============================
----------------------------------
+================================
 • Get as close as possible to 21
-=================================
-''')
-      
+================================
+'''
+as_card = [1,11]
+num = [2,3,4,5,6,7,8,9,10,10,10,as_card]
+user_num = [random.choice(num) for i in range(1)]
+dealer_num = []
+
 def end_detail(user_num, dealer_num):
     print(f"Your final cards: {user_num}, current score = {sum(user_num)}")
     print(f"Dealer's final cards: {dealer_num}, current score = {sum(dealer_num)}")
@@ -18,31 +21,39 @@ def start_again():
     os.system('clear')
     return is_continue
 
-num = [1,2,3,4,5,6,7,8,9,10]
-user_num = [random.choice(num) for i in range(1)]
-dealer_num = [random.choice(num) for i in range(1)]
+def as_card_function(user):
+    if as_card in user:
+        user.remove(as_card)
+        if as_card[1] + sum(user) > 21:
+            user.append(1)
+        else: user.append(11)
 
 
 is_continue = True if input("Do you want to play The Blackjack ('y' or 'n'): ") == "y" else False
+os.system('clear')
 while is_continue:
+    print(text)
     user_num += [random.choice(num) for i in range(1)]
+    as_card_function(user_num)
     if sum(user_num) > 21:
         end_detail(user_num, dealer_num)
         print("You went over, you lose!")
         if start_again():
             user_num = [random.choice(num) for i in range(1)]
-            dealer_num = [random.choice(num) for i in range(1)]
+            dealer_num = []
             continue
         break
-    dealer_num += [random.choice(num) for i in range(1)]
-    if sum(dealer_num) > 21:
+    if sum(user_num) == 21:
         end_detail(user_num, dealer_num)
-        print("The dealer went over, you win!")
+        print("BLACKJACK!!\n You!")
         if start_again():
             user_num = [random.choice(num) for i in range(1)]
-            dealer_num = [random.choice(num) for i in range(1)]
-            continue 
-        break 
+            dealer_num = []
+            continue
+        break
+
+    dealer_num = [random.choice(num) for i in range(2)]
+    as_card_function(dealer_num)
 
     print(f"Your cards: {user_num}, current score = {sum(user_num)}")
     print(f"Dealer's first card: {dealer_num[0]}")
@@ -51,10 +62,17 @@ while is_continue:
     os.system('clear')
 
     if not is_continue:
-        if sum(user_num) == sum(dealer_num):
+        while sum(dealer_num) < 17:
+            dealer_num += [random.choice(num) for i in range(1)]
+            as_card_function(dealer_num)
+        
+        if sum(dealer_num) > 21:
+            end_detail(user_num, dealer_num)
+            print("Dealer went over, you win!")
+        elif sum(user_num) == sum(dealer_num):
             end_detail(user_num, dealer_num)
             print("Tie!")
-        if sum(user_num) > sum(dealer_num):
+        elif sum(user_num) > sum(dealer_num):
             end_detail(user_num, dealer_num)
             print("You win!")
         else:
@@ -63,5 +81,5 @@ while is_continue:
 
         if start_again():
             user_num = [random.choice(num) for i in range(1)]
-            dealer_num = [random.choice(num) for i in range(1)]
-            continue
+            dealer_num = []
+            is_continue = True
