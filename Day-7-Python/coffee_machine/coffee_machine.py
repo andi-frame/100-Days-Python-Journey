8491e9bfd7b2399a383290f0f22a9ebb7610ff85
andi-frame <132210040+andi-frame@users.noreply.github.com>
2023-05-07 23:55:59 +0800

    Update coffee_machine.py => fixing some bugs

--- Day-7-Python/coffee_machine/coffee_machine.py
+++ Day-7-Python/coffee_machine/coffee_machine.py
@@ -31,12 +31,26 @@ menu = {
     }
     }
     
-
-water = 300.00
-milk = 200.00
-coffee = 100.00
+init_ingredients ={
+    'water' : 300.00,
+    'milk' : 200.00,
+    'coffee' : 100.00,
+}
 money = 0.00
 customer = 0
+
+
+def is_sufficient(drink):
+    for ingredient in menu[drink]:
+        if (menu[drink][ingredient] < init_ingredients[ingredient]):
+            print(f"Sorry there is not enough {ingredient}\n")
+    for ingredient in menu[drink]:
+        if (menu[drink][ingredient] < init_ingredients[ingredient]):
+            return False
+    return True
+
+
+
 
 print(text)
 is_continue = True
@@ -47,9 +61,9 @@ while is_continue:
 
     if user_input == 'report':
         print("Current resource values:")
-        print(f"Water: {water}ml")
-        print(f"Milk: {milk}ml")
-        print(f"Coffee: {coffee}g")
+        print(f"Water: {init_ingredients['water']}ml")
+        print(f"Milk: {init_ingredients['milk']}ml")
+        print(f"Coffee: {init_ingredients['coffee']}g")
         print(f"Money: ${money}")
 
     elif user_input == 'off':
@@ -57,16 +71,8 @@ while is_continue:
         break
 
     elif user_input == 'espresso' or user_input == 'latte' or user_input == 'cappucino':
-        if water < menu[user_input]['water']:
-            print("Sorry there is not enough water\n")
-            continue
-        if milk < menu[user_input]['milk']:
-            print("Sorry there is not enough milk\n")
-            continue
-        if coffee < menu[user_input]['coffee']:
-            print("Sorry there is not enough coffee\n")
-            continue
-
+        if not is_sufficient(user_input):
+            break
         print(f"You choose {user_input}, it cost ${menu[user_input]['money']}.")
         print("Please, insert your coin!")
         pennies = int(input("How many pennies: "))
@@ -84,6 +90,9 @@ while is_continue:
             money += user_money
             print(f"Here is your {user_input} coffee. Enjoy!!")
             print(f"Here is ${round((user_money - menu[user_input]['money']), 2)} dollars in change.\n")
+            init_ingredients['water'] -= menu[user_input]['water']
+            init_ingredients['milk'] -= menu[user_input]['milk']
+            init_ingredients['coffee'] -= menu[user_input]['coffee']
             continue
 
         if user_money < menu[user_input]['money']:
