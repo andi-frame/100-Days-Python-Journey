9b2aedf38e76d72ab0188dd7e356c39d1833e973
andi-frame <132210040+andi-frame@users.noreply.github.com>
2023-05-13 21:52:51 +0700

    Update snack_game_main.py

--- Day-13-Python/snack_game_main.py
+++ Day-13-Python/snack_game_main.py
@@ -19,16 +19,29 @@ scoreboard = Scoreboard(score)
 game_is_on = True
 food_exist = True
 while game_is_on:
-    
+
+    # Detect collision with food
     if not food_exist:
         food = Food()
     if snack.all_snack[0].distance(food) <= 15 :
-        snack.len_plus()
+        snack.len_plus(snack.all_snack[-1].pos())
         score += 1
         scoreboard.clear()
         scoreboard = Scoreboard(score)
         food.goto(x = random.randint(-230, 230), y = random.randint(-230, 230))
+
+    # Detect collision with walls
+    if snack.all_snack[0].xcor() > 230 or snack.all_snack[0].xcor() < -230 or snack.all_snack[0].ycor() > 230 or snack.all_snack[0].ycor() < -230:
+        game_is_on = False
+        scoreboard.game_over()
     
+    # Detect collision with tail
+    for i in snack.all_snack[1:]:
+        if snack.all_snack[0].distance(i) <= 10:
+            game_is_on = False
+            scoreboard.game_over()
+    
+    # Control The Snack
     time.sleep(0.1)
     screen.update()
     snack.move()
