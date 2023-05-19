8d8312a64c0a7d43eb04daa061a9e270379c7a12
andi-frame <132210040+andi-frame@users.noreply.github.com>
2023-05-19 09:21:26 +0700

    Update main.py

--- Day-19-Python/main.py
+++ Day-19-Python/main.py
@@ -1,15 +1,27 @@
-import tkinter
+from tkinter import * #type: ignore
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Tkinter GUI Program")
 window.minsize(width = 500, height = 300)
 
-label = tkinter.Label(text = "Hello", font=("Arial", 25, "bold"))
-label.pack()
+# Label
+label = Label(text = "Button not clicked", font=("Arial", 25, "bold"))
+label.grid(row = 1, column = 1)
+
+# Button
+def button_clicked():
+    user_input = input.get()
+    label["text"] = user_input
 
 
+input = Entry(width = 15)
+input.grid(row = 3, column = 4)
 
+button = Button(text = "click me!", command = button_clicked)
+button.grid(row = 2, column = 2)
 
+new_button = Button(text = "new button!")
+new_button.grid(row = 1, column = 3)
 
 
 window.mainloop()
