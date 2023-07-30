ab2782a253afcf7b9c47d519dfd82d9f528423a4
andi-frame <andifarhan1094@gmail.com>
2023-07-31 00:21:50 +0800

    Update the UI

--- Day-27-Python/ui.py
+++ Day-27-Python/ui.py
@@ -16,7 +16,7 @@ class QuizIterface:
 
         # Quiz
         self.quiz_canvas = Canvas(height=250, width = 300)
-        self.quiz_canvas.create_text(150, 125, text = f"{self.quiz}", font = ("Arial", 10, "italic"))
+        self.quiz_canvas.create_text(150, 125, text = f"{self.quiz}", font = ("Arial", 10, "italic"), width = 280)
         self.quiz_canvas.grid(column = 1, columnspan=2, row = 2, padx = 20, pady = 20)
 
         # Button
